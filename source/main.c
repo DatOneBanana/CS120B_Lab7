e7094bef1aac41edf40452a25db280263385a811
Ubuntu <datonearash@gmail.com>
2020-11-11 23:37:25 -0800

    fixed portd not working

--- source/main.c
+++ source/main.c
@@ -20,7 +20,7 @@ int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
-	DDRB = 0xFF; PORTD = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
     /* Insert your solution below */
 	ADC_init();
 	unsigned short x = 0;
